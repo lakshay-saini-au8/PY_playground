2d54782fb5d1675da9f8dabba1258b331fb93cf6
Lakshay saini <lakshaysaini2013@gmail.com>
2020-08-28 11:47:18 +0530

    python day05

---
+++ random/day05.py
@@ -0,0 +1,33 @@
+# question 1
+'''
+Upward Star Triangle with n lines (n: I/P) (Eg: n=3)
+*
+* *
+* * *
+'''
+
+n = int(input("enter you no: "))
+# for i in range(n):
+#     print("* "*(i+1))
+
+# question 2
+'''
+Upward centered (Eg: n=3)
+
+     *
+    * *
+   * * *
+'''
+# for i in range(n):
+#     s = ("* "*(i+1)).center(2*n, " ")
+#     print(s)
+
+# question 3
+'''
+Kaju Katli (For odd I/P only)(Eg:n=3)
+     *
+    * *
+   * * *
+    * *
+     *
+'''
