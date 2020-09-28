b058e7b94dfdf8196a5938bb3810817ced04d50e
Lakshay saini <lakshaysaini2013@gmail.com>
2020-09-28 23:47:14 +0530

    hacker rank py

--- hackerrank/python/alphabet_rangoli.py
+++ hackerrank/python/alphabet_rangoli.py
@@ -1,3 +1,32 @@
-from itertools import permutations
+def print_rangoli(size):
+    start = ord("a")-1
+    # your code goes here
 
-print(list(permutations(['1', '2', '3'], 2)))
+    for i in range(size):
+        str_new = ""
+        for j in range((size-i)*2-2):
+            str_new = str_new+"-"
+        for k in range(i+1):
+            str_new = str_new + chr(start+(size-k))+"-"
+        for m in range(i, 0, -1):
+            str_new = str_new+chr(start+1+(size-m))+"-"
+        for l in range((size-i)*2-2):
+            str_new = str_new+"-"
+
+        print(str_new[:-1])
+    size = size - 1
+    for i in range(size, 0, -1):
+        str_new = ""
+        for j in range((size-i)*2+2):
+            str_new = str_new+"-"
+        for k in range(i):
+            str_new = str_new + chr(start+(size+1-k))+"-"
+        for m in range(i-1, 0, -1):
+            str_new = str_new+chr(start+1+(size+1-m))+"-"
+        for l in range((size-i)*2+2):
+            str_new = str_new+"-"
+
+        print(str_new[:-1])
+
+
+print_rangoli(5)
